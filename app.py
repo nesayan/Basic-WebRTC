9909141f76b1afbfa52d3d4bc8876fe566c2bb09
nesayan <sahasayan100@gmail.com>
2021-03-29 00:35:30 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template, make_response, redirect
-from flask_socketio import SocketIO, send, emit
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, join_room, leave_room
 import os
+
+ROOM = "room"
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+clients = []
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on("data")
-def handleMessage(data):
-    emit("data",data,broadcast=True)
-    
+@socket.on('connect')
+def conn():
+    clients.append(request.sid)
+    emit('ready', room=ROOM, skip_sid=request.sid)
+    join_room(room=ROOM,sid=request.sid)
+    print("[ CLIENT CONNECTED ]", request.sid)
+
+@socket.on('disconnect')
+def disconn():
+    clients.remove(request.sid)
+    leave_room(room=ROOM, sid=request.sid)
+    print("[ CLIENT DISCONNECTED ]", request.sid)
+
+@socket.on('data')
+def emitData(data):
+    print('Message from {}: {}'.format(request.sid, data))
+    emit('data', data, room=ROOM, skip_sid=request.sid)
+
 if __name__ == "__main__":
     socketio.run(app)
