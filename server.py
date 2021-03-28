800113d6c0658ae382fbae512bc39e79bcb19ea0
nesayan <sahasayan100@gmail.com>
2021-03-28 19:50:46 +0530

    Initial commit

--- server.py
+++ server.py
@@ -1,40 +1,18 @@
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
-ROOM = "room"
-
-#sio = socketio.AsyncServer(cors_allowed_origins="*")
-#app = web.Application()
-#sio.attach(app)
-app = Flask(__name__)
-socket = SocketIO(app, cors_allowed_origins="*")
 clients = []
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def index():
-    return render_template('client.html')
-
-@socket.on('connect')
-def conn():
-    clients.append(request.sid)
-    emit('ready', room=ROOM, skip_sid=request.sid)
-    join_room(room=ROOM,sid=request.sid)
-    print("[ CLIENT CONNECTED ]", request.sid)
-
-@socket.on('disconnect')
-def disconn():
-    clients.remove(request.sid)
-    leave_room(room=ROOM, sid=request.sid)
-    print("[ CLIENT DISCONNECTED ]", request.sid)
+    return render_template('index.html')
 
 @socketio.on("message")
 def handleMessage(data):
     emit("new_message",data,broadcast=True)
-
+    
 if __name__ == "__main__":
-    #web.run_app(app, host= "127.0.0.1", port= 5000)
-    socket.run(app, debug=False, host='0.0.0.0', port=5004)
-
-
-
-
+    socketio.run(app, debug=True, host='0.0.0.0', port=5004)
